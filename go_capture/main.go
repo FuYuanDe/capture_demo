69cb29b2fedad5222de47f3b5b6bc410a1806479
mason <fuyuande@dinstar.net>
2018-07-07 17:07:54 +0800

    1. 抓包服务器代码优化

--- go_capture/main.go
+++ go_capture/main.go
@@ -17,11 +17,41 @@ import (
 	"github.com/google/gopacket/pcapgo"
 )
 
-func CaptureSaveFile() {
-	return
+type CaptureManager struct {
+	F    *os.File       //抓包文件
+	W    *pcapgo.Writer //用于写文件
+	conn *net.UDPConn   //udp连接
 }
 
+func CaptureSaveFile() {
+	for {
+		//套接字接收缓存
+		buffer := make([]byte, 65535)
+
+		//接收数据
+		num, _, err := CapMng.conn.ReadFromUDP(buffer)
+		if nil != err {
+			continue
+		} else {
+			captureInfo := gopacket.CaptureInfo{
+				Timestamp:      time.Now().UTC(),
+				CaptureLength:  num,
+				Length:         num,
+				InterfaceIndex: 0,
+			}
+			//写入pcap数据包头以及数据
+			CapMng.W.WritePacket(captureInfo, buffer[:num])
+			CapMng.F.Seek(0, os.SEEK_END)
+		}
+	}
+}
+
+var CapMng *CaptureManager
+
 func main() {
+	//全局管理控制块
+	CapMng = &CaptureManager{}
+
 	//创建目录
 	os.MkdirAll("capture_packet", 0777)
 
@@ -30,11 +60,11 @@ func main() {
 	fil := "capture_packet" + "/" + "capture" + "_" + t.Format("20060102150405") + ".pcap"
 
 	//创建文件
-	F, _ := os.Create(fil)
+	CapMng.F, _ = os.Create(fil)
 
 	//填充wireshark头
-	W := pcapgo.NewWriter(F)
-	W.WriteFileHeader(1024, layers.LinkTypeEthernet)
+	CapMng.W = pcapgo.NewWriter(CapMng.F)
+	CapMng.W.WriteFileHeader(1024, layers.LinkTypeEthernet)
 
 	//设置监听地址
 	Addr, err := net.ResolveUDPAddr("udp", "0.0.0.0:"+strconv.Itoa(8099))
@@ -44,32 +74,13 @@ func main() {
 		return
 	} else {
 		//创建udp连接
-		Conn, err := net.ListenUDP("udp", Addr)
+		CapMng.conn, err = net.ListenUDP("udp", Addr)
 		if nil != err {
 			fmt.Println("create conn fail")
 			return
 		} else {
 			fmt.Println("Capture Start")
-			for {
-				//套接字接收缓存
-				buffer := make([]byte, 65535)
-
-				//接收数据
-				num, _, err := Conn.ReadFromUDP(buffer)
-				if nil != err {
-					continue
-				} else {
-					captureInfo := gopacket.CaptureInfo{
-						Timestamp:      time.Now().UTC(),
-						CaptureLength:  num,
-						Length:         num,
-						InterfaceIndex: 0,
-					}
-					//写入pcap数据包头以及数据
-					W.WritePacket(captureInfo, buffer[:num])
-					F.Seek(0, os.SEEK_END)
-				}
-			}
+			go CaptureSaveFile()
 		}
 	}
 
@@ -88,7 +99,8 @@ func main() {
 	default:
 	}
 	fmt.Println("*************Server Recive signal %s, program exit", sig.String())
-	F.Close()
+	CapMng.F.Close()
+	CapMng.conn.Close()
 	fmt.Println("CLOSE")
 	time.After(1 * time.Second)
 }
